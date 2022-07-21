80526033f0dd251e50b410572214a4a97aa6c5ff
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-07-21 14:42:47 +0300

    Update Feedback.js

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -2,14 +2,25 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import css from './Feedback.module.css';
 class Feedback extends React.Component {
+    handleGoodFeedback = event => {
+
+        const { target } = event;
+    }
+    handleNeutralFeedback = event => {
+const { target } = event;
+    }
+    handleBadFeedback = event => {
+const { target } = event;
+    }
+
     render() {
         return (
 <section className={css.feedback}>
         {<h2 className={css.title}>Please leave feedback</h2>}
             <div className={css.btnBlock}>
-                    <button onClick={()=> (console.log("rklvlksdfl;a")) } className={css.btn} type='button'>Good</button>
-                    <button className={css.btn} type='button'>Neutral</button>
-                    <button className={css.btn} type='button'>Bad</button>
+                    <button onClick={this.handleGoodFeedback} className={css.btn} type='button'>Good</button>
+                    <button onClick={this.handleNeutralFeedback} className={css.btn} type='button'>Neutral</button>
+                    <button onClick={this.handleBadFeedback} className={css.btn} type='button'>Bad</button>
             </div>
             
         {<h2 className={css.title}>Statistics</h2>}
